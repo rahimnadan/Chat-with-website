3b5c5ceb5d41e02324cde931caba2044f0ee809a
Abdur Rahim <92539299+rahimnadan@users.noreply.github.com>
2024-10-31 10:21:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def main():
     user_question = st.chat_input("Ask a question (query/prompt)")
 
     if user_question and url:
-        os.environ['GOOGLE_API_KEY'] = "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"  
+        os.environ['GOOGLE_API_KEY'] = "AIzaSyBkGm1xpVH77IMzbLwAfN23gEj_8uGQSxo"  
         
         
         r = requests.get(url)
@@ -71,7 +71,7 @@ def main():
         retriever = vectordb.as_retriever(search_kwargs={"k": 3})
 
         
-        llm = ChatGroq(model="llama3-70b-8192", groq_api_key="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")
+        llm = ChatGroq(model="llama3-70b-8192", groq_api_key="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")
 
         # Create a RetrievalQA instance from the model and retriever
         qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
